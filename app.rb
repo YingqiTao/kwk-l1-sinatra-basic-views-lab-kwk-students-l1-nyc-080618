07840dd20e6fc6a803d7af780ddf08cad583f53a
YingqiTao <yingqitao2000@gmail.com>
2018-08-14 18:10:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-
+  
+  get '/' do
+    erb :index
+  end
+  
+  get '/marnie' do
+    erb :marnie
+  end
+  
+  get '/groupinfo' do
+    erb :group_name
+  end
 end
